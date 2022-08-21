48433025785ec136e5864b2e60bc1a82ff2f7c20
Jacky <acetimelemon@gmail.com>
2022-08-22 03:52:03 +0800

    modified:   Flask.py 	new file:   MultiMsgSync.py 	new file:   __pycache__/MultiMsgSync.cpython-310.pyc 	new file:   __pycache__/tools.cpython-310.pyc 	renamed:    static/css/keyboard.css -> static/css/deploy.css 	modified:   static/js/msg.js 	modified:   templates/teleop.html 	new file:   tools.py

--- static/js/msg.js
+++ static/js/msg.js
@@ -1,1 +1,39 @@
 
+$(function () {
+    $("#mybutton").click(function (event) {
+        $.getJSON('/SomeFunction', {},
+            function (data) { }); return false;
+    });
+});
+
+var videoc = document.querySelector('.video>img')
+
+
+function rgb_Video(){
+    document.getElementById("videoc").src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/800px-A_black_image.jpg";
+    document.getElementById("videoc").src="/video_feed";
+    
+}
+function head_Orientation(){
+    document.getElementById("videoc").src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/800px-A_black_image.jpg";
+    document.getElementById("videoc").src="/dect_video";
+
+}
+function close_video(){
+    location.reload();
+}
+ps4_on.onclick = function () {
+
+}
+
+ps4_off.onclick = function () {
+
+}
+
+raidarOff.onclick = function () {
+
+}
+
+raidarOn.onclick = function () {
+
+}
